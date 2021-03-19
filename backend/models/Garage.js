2b95eb07710c06d44b0e9ec59eb99dfc59bf84ae
Aryan Jha <aryanjha@gmail.com>
2021-03-19 14:01:48 -0400

    updated garage and user schemas

--- backend/models/Garage.js
+++ backend/models/Garage.js
@@ -1,15 +1,39 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// need to find better address regex.
+const addressRegex = /A-Za-z0-9'\.\-\s\,/;
+
+
 const GarageSchema = new Schema({
-    spotsCount: {
+    name: {
+        type: String,
+        required: true
+    },
+    numberSpots: {
         type: Number,
         required: true
     },
-    notifications: [{
-        type: ObjectId,
+    location: {
+        type: String,
+        match: addressRegex,
         required: true
-    }]
+    },
+    spotsArray: [{
+        level: {
+            type: String,
+            required: true
+        },
+        spotNumber: {
+            type: String,
+            required: true
+        },
+        isOpen: {
+            type: Boolean,
+            required: true
+        }
+    }
+    ]
 })
 
 module.exports = Garage = mongoose.model("garage", GarageSchema);
