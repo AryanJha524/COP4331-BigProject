df84472a1cb95bceeaeabde3ab503ae8982fc477
rossrellis <rrellis2309@knights.ucf.edu>
2021-04-16 22:05:42 -0400

    Destination.js

---
+++ mobile/mobile_app/components/Destination.js
@@ -0,0 +1,150 @@
+const styles = StyleSheet.create({
+    root: {
+        position: 'absolute',
+        width: '206px',
+        height: '392px',
+        background: '#030303',
+    },
+
+    ParkyLogoBox: {
+        position: 'absolute',
+        width: '144px',
+        height: '42px',
+        fontFamily: 'Sriracha',
+        fontStyle: 'normal',
+        fontWeight: 'normal',
+        fontSize: '24px',
+        lineHeight: '42px',
+        textAlign: 'center',
+        textDecorationline: 'underline',
+        color: '#F8F0F0',
+    },
+
+    ParkyCarBackgroundVector: {
+        position: 'absolute',
+        left: '0%',
+        right: '0%',
+        top: '0%',
+        bottom: '0%',
+    },
+
+    ParkyCarVector: {
+        position: 'absolute',
+        left: '12.5%',
+        right: '12.5%',
+        top: '0%',
+        bottom: '33.33%',
+        background: '#F8C615',
+    },
+
+    Outline: {
+        position: 'relative',
+        width: '328px',
+        height: '72px',
+    },
+
+    DestinationAddress: {
+        position: 'absolute',
+        width: '114px',
+        height: '16px',
+        left: '13px',
+        top: '20px',
+        fontFamily: 'Roboto',
+        fontStyle: 'normal',
+        fontWeight: 'normal',
+        fontSize: '12px',
+        lineHeight: '16px',
+        letterSpacing: '0.4px',
+        color: '#BB86FC',
+    },
+
+    TextFieldLabel: {
+        position: 'absolute',
+        width: '169.61px',
+        height: '2px',
+        background: '#BB86FC',
+    },
+
+    TextFieldSurface: {
+        position: 'absolute',
+        width: '180.77px',
+        height: '52px',
+        background: '#BB86FC',
+        borderRadius: '2px',
+    },
+
+    TextFieldBorder: {
+        width: '183px',
+        height: '56px',
+        background: '#BB86FC',
+        borderRadius: '4px',
+    },
+
+    UseLocationLabel: {
+        position: 'absolute',
+        width: '84.71px',
+        height: '32px',
+
+        fontFamily: 'Roboto',
+        fontStyle: 'normal',
+        fontWeight: '500',
+        fontSize: '12px',
+        lineHeight: '16px',
+
+        textAlign: 'center',
+        letterSpacing: '1.25px',
+        textTransform: 'uppercase',
+        color: '#000000',
+        transform([{ rotateX: '0.18deg' },
+    },
+
+    Elevation1: {
+        position: 'absolute',
+        height: '40px',
+        left: '-15.91%',
+        right: '22.73%',
+        top: calc(50 % - 40px / 2 + 703px),
+
+        background: '#E4EEED',
+        boxShadow: 0px 4px 5px rgba(0, 0, 0, 0.14), 0px 1px 10px rgba(0, 0, 0, 0.12), 0px 2px 4px rgba(0, 0, 0, 0.2),
+        borderRadius: '28px',
+        transform: matrix(1, 0, 0, -1, 0, 0),
+    },
+
+    Elevation2: {
+        position: 'absolute',
+        height: '40px',
+        left: '-15.91%',
+        right: '22.73%',
+        top: calc(50 % - 40px / 2 + 100px),
+
+        background: '#E4EEED',
+        boxShadow: 0px 4px 5px rgba(0, 0, 0, 0.14), 0px 1px 10px rgba(0, 0, 0, 0.12), 0px 2px 4px rgba(0, 0, 0, 0.2),
+        borderRadius: '28px',
+    },
+
+    Elevation3: {
+        position: 'absolute',
+        height: '40px',
+        left: '-15.91%',
+        right: '22.73%',
+        top: calc(50 % - 40px / 2 + 20px),
+
+        background: '#E4EEED',
+        boxShadow: 0px 4px 5px rgba(0, 0, 0, 0.14), 0px 1px 10px rgba(0, 0, 0, 0.12), 0px 2px 4px rgba(0, 0, 0, 0.2),
+        borderRadius: '28px',
+        transform: matrix(1, 0, 0, -1, 0, 0),
+    },
+
+    States: {
+        position: 'absolute',
+        visibility: 'hidden',
+        left: '0%',
+        right: '0%',
+        top: '0%',
+        bottom: '0%',
+        background: '#000000',
+        borderRSadius: '23px',
+    },
+
+})
