be1f7bca3b992d521ad5cfe87db33d38d799b1ab
HenryP21 <henrydavidpinzon@knights.ucf.edu>
2021-04-17 22:35:25 -0400

    login/register adjusted, added password reset

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,9 +1,9 @@
 import React, { Component, useState } from 'react';
-import { Router, Switch, Route } from "react-router-dom";
+import { Router, Switch, Route, useHistory } from "react-router-dom";
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import Home from './pages/Home';
-import SignIn from './pages/PreferencesPage';
+import NewPref from './pages/newPref';
 import Dashboard from './pages/Dashboard';
 import { AuthProvider } from './Auth';
 import PrivateRoute from './PrivateRoute';
@@ -13,17 +13,18 @@ import history from './history.js';
 export default class Routes extends Component {
     render(){
         return (
-          <AuthProvider>
             <Router history={history}>
+              <AuthProvider>
                 <Switch>
                 <Route exact path="/" component={Home}/>
                 <Route exact path="/login" component={LoginPage} />
                 <Route exact path="/register" component={RegisterPage} />
                 <Route exact path="/forgot-password" component={ForgotPassword} />
+                <Route exact path="/pref" component={NewPref} />
                 <PrivateRoute exact path="/dashboard" component={Dashboard}/> 
                 </Switch>
+                </AuthProvider>
             </Router>
-          </AuthProvider>
         )
     }
 }
--- frontend/src/pages/ForgotPassword.js
+++ frontend/src/pages/ForgotPassword.js
@@ -1,12 +1,48 @@
 import React, { useRef, useState } from 'react';
-import { Button, CssBaseline, Container, Typography, Grid, TextField} from '@material-ui/core';
+import { Button, CssBaseline, Container, Typography, Grid, TextField, AppBar, Toolbar} from '@material-ui/core';
+import DriveEtaIcon from '@material-ui/icons/DriveEta'
 import { Alert } from '@material-ui/lab'
 import { useAuth } from '../Auth';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link, useHistory } from "react-router-dom";
-import fire from './../fire.js';
+import fire, { auth } from './../fire.js';
 
 const useStyles = makeStyles((theme) => ({
+    root:{
+      display: 'flex',
+      justifyContent: "center",
+      textAlign: "center",
+      alignItems: "center",
+      height: "100vh",
+      color: 'white',
+      minheight: '70vh',
+      // backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/carLoginPic.jpg'})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      background: 'gold',
+
+  },
+  appbar:{
+      background: 'black',
+      fontFamily: 'Roboto',
+  },
+  wrapper: {
+      width: '80%',
+      margin: '0 auto',
+  },
+  appbarTitle:{
+      flexGrow: '1', 
+  },
+  colorText: {
+      color: '#fff',
+  },
+  title: {
+      color: '#fff',
+      fontSize: '4.5rem',
+  },
+  icon: {
+    alignItems: 'center',
+  },
     paper: {
       marginTop: theme.spacing(8),
       display: 'flex',
@@ -30,27 +66,36 @@ export default function ForgotPassword()
 {
     const classes = useStyles();
     const history = useHistory();
-    const emailRef = useRef();
-    //const { resetPassword } = useAuth();
+    const [email, setEmail] = useState(null);
+    const [emailHasBeenSent, setEmailHasBeenSent] = useState(false);
     const [error, setError] = useState("");
     const [message, setMessage] = useState("");
     const [loading, setLoading] = useState(false);
 
-    function resetPassword(email)
-    {
-      return fire.auth().sendPasswordResetEmail(email);
-    }
+    // const sendResetEmail = e => {
+    //   e.preventDefault();
+    //   auth.sendPasswordResetEmail(email)
+    //   .then(() => {
+    //     setEmailHasBeenSent(true);  
+    //     setTimeout(() => {setEmailHasBeenSent(false)}, 3000);
+    //   })
+    //   .catch(() => {
+    //     setError("Error resetting password");
+    //   });
 
-    async function handleSubmit(event)
+    // };
+
+    async function sendResetEmail(event)
     {
         event.preventDefault();
+        console.log(email);
 
         try
         {
             setMessage("");
             setError("");
             setLoading(true);
-            await resetPassword(emailRef.current.value);
+            auth.sendPasswordResetEmail(email);
             setMessage("Check your inbox to reset your password");
         } catch {
             setError("Failed to reset Password");
@@ -61,6 +106,17 @@ export default function ForgotPassword()
     return (
         <Container component="main" maxWidth="xs">
         <CssBaseline />
+        <AppBar className={classes.appbar} elevation={0}>
+                <Toolbar className={classes.wrapper}>
+                <h1 className={classes.appbarTitle}>
+                    <span className={classes.colorText}>
+                      Parky
+                    </span>
+                    <DriveEtaIcon className={classes.icon}/>
+                </h1>
+               </Toolbar> 
+            </AppBar>
+            <br/>
         <div className={classes.paper}>
           <Typography component="h1" variant="h5">
             Password Reset
@@ -73,7 +129,7 @@ export default function ForgotPassword()
               margin="normal"
               required
               fullWidth
-              ref={emailRef}
+              onChange={(e) => setEmail(e.target.value)}
               label="Email Address"
               name="email"
               autoComplete="email"
@@ -85,7 +141,7 @@ export default function ForgotPassword()
               variant="contained"
               color="primary"
               className={classes.submit}
-              onClick={handleSubmit}
+              onClick={sendResetEmail}
             >
               Reset Password
             </Button>
